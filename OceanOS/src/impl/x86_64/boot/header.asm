90f9045df6d5f95cd8da04a23c4feaacc7f1cea0
Bkiser2000 <155510865+Bkiser2000@users.noreply.github.com>
2025-07-15 19:56:18 -0600

    Add files via upload

--- OceanOS/src/impl/x86_64/boot/header.asm
+++ OceanOS/src/impl/x86_64/boot/header.asm
@@ -1,13 +1,14 @@
 section .multiboot_header
+align 8
 header_start:
     ; magic number
-    dd 0xe85258d6 ; multiboot2
+    dd 0xe85250d6 ; multiboot2
     ; architecture  
     dd 0 ; protected mode i386
     ; header length
     dd header_end - header_start
     ; checksum
-    dd 0x100000000 - (0xe85258d6 + 0 + (header_end - header_start))
+    dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))
 
     ; required end tag
     dw 0    ; type
--- OceanOS/src/intf/print.h
+++ OceanOS/src/intf/print.h
@@ -25,4 +25,4 @@ enum {
 void print_clear();
 void print_char(char character);
 void print_str(char* string);
-void print_set_color(unit8_t foreground, unit8_t background);
+void print_set_color(uint8_t foreground, uint8_t background);
